53833ed7ed82eac013709fda8e56062bf5e99226
Caro <Caro@10.68.172.243>
2015-04-07 09:34:50 -0600

    Primera subida de archivos

---
+++ prueba/src/modelo/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Caro
+ */
+public class Cliente {
+    
+    private String nombre;
+    private String cedula;
+    private String direccion;
+
+    public Cliente(String nombre, String cedula, String direccion) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.direccion = direccion;
+    }
+
+    public Cliente() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
+}
